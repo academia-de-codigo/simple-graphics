bb3386479638e9a37d72bc54893ec6703c1fff15
Rui Ferrão <rui.ferrao@academiadecodigo.org>
2015-09-25 08:49:20 +0100

    updated example usage

--- src/org/academiadecodigo/simplegraphics/test/Tester.java
+++ src/org/academiadecodigo/simplegraphics/test/Tester.java
@@ -9,16 +9,10 @@ import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 
 public class Tester implements KeyboardHandler {
 
-    static int cols = 100;
-    static int rows = 100;
-    static int cellSize = 5;
-
     public static void main(String[] args) {
 
         Tester t = new Tester();
         t.test();
-
-
 
     }
 
@@ -30,33 +24,11 @@ public class Tester implements KeyboardHandler {
         event.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
         k.addEventListener(event);
 
-        Rectangle field = new Rectangle(10, 10, cols * cellSize, rows * cellSize);
-        field.setColor(Color.BLACK);
-        field.draw();
-
-        drawCar(0, 0);
+        Rectangle rect = new Rectangle(10, 10, 300 , 300);
+        rect.setColor(Color.BLACK);
+        rect.draw();
 
 
-
-    }
-
-    public void drawCar(int col, int row) {
-
-        int xOrig = col * cellSize + 10;
-        int yOrig = row * cellSize + 10;
-
-        Rectangle car = new Rectangle(xOrig, yOrig, xOrig + cellSize, yOrig + cellSize);
-        car.setColor(Color.BLUE);
-        car.fill();
-    }
-
-    public void deleteCar(int col, int row) {
-        int xOrig = col * cellSize + 10;
-        int yOrig = row * cellSize + 10;
-
-        Rectangle car = new Rectangle(xOrig, yOrig, xOrig + cellSize, yOrig + cellSize);
-        car.setColor(Color.WHITE);
-        car.fill();
     }
 
     @Override
