50602de455e3f854c97c0efd33d9ce0a30ed368e
miltinhoc <miltonamc@gmail.com>
2020-02-11 20:08:02 -0800

    added mouse press and release, right and left mouse click

--- src/main/java/org/academiadecodigo/simplegraphics/mouse/Mouse.java
+++ src/main/java/org/academiadecodigo/simplegraphics/mouse/Mouse.java
@@ -24,37 +24,36 @@ public class Mouse implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        handler.mouseClicked(new org.academiadecodigo.simplegraphics.mouse.MouseEvent(e.getX(), e.getY()));
+        handler.mouseClicked(new org.academiadecodigo.simplegraphics.mouse.MouseEvent(e.getX(), e.getY(), e.getButton()));
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        handler.mouseMoved(new org.academiadecodigo.simplegraphics.mouse.MouseEvent(e.getX(), e.getY()));
+        handler.mouseMoved(new org.academiadecodigo.simplegraphics.mouse.MouseEvent(e.getX(), e.getY(), e.getButton()));
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-
+        handler.mousePressed(new org.academiadecodigo.simplegraphics.mouse.MouseEvent(e.getX(), e.getY(), e.getButton()));
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-
+        handler.mouseReleased(new org.academiadecodigo.simplegraphics.mouse.MouseEvent(e.getX(), e.getY(), e.getButton()));
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-
+        handler.mouseEntered(new org.academiadecodigo.simplegraphics.mouse.MouseEvent(e.getX(), e.getY(), e.getButton()));
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-
+        handler.mouseExited(new org.academiadecodigo.simplegraphics.mouse.MouseEvent(e.getX(), e.getY(), e.getButton()));
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
-
+        handler.mouseDragged(new org.academiadecodigo.simplegraphics.mouse.MouseEvent(e.getX(), e.getY(), e.getButton()));
     }
-
 }
